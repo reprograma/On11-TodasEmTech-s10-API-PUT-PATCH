0e57b4c44f4a87b5169ffd28fd604af459a0bb44
Raphaella <raphaa_leite@hotmail.com>
2021-05-13 20:36:43 -0300

    Exercicio semana10

--- servidor-paraolar/to-do-server/src/app.js
+++ servidor-paraolar/to-do-server/src/app.js
@@ -1,14 +1,24 @@
-const express = require("express")
-const app = express()
-const cors = require("cors")
+const  express  =  require ( "express" )
+const  app  =  express ( )
+const  cors  =  require ( "cors" )
+const  express  =  require ( "express" ) ;
+const  app  =  express ( ) ;
+const  cors  =  require ( "cors" ) ;
 
-app.use(cors()) //resolve o problema de uma só origem do navegador
-app.use(express.json()) //transforma em json o body (que vem em strig)
+app.use ( cors ( ) )  
+app.use ( express.json ( ) )  
+app.use ( cors ( ) ) ;  
+app.use ( express.json ( ) ) ;  
 
-const index = require("./routes/index")
-const tarefas = require("./routes/toDoRoutes")
+const índice =  requer ( "./routes/index" )
+const tarefas  =  exigir ( "./routes/toDoRoutes" ) 
+const índice =  requer ( "./routes/index" ) ;
+const tarefas  =  exigir ( "./routes/toDoRoutes" ) ;
 
-app.use("/", index)
-app.use("/tarefas", tarefas)
+app.use ( "/" ,  índice )
+app.use ( "/ tarefas" ,  tarefas )
+app.use ( "/" ,  índice ) ;
+app.use ( "/ tarefas" ,  tarefas ) ;
 
-module.exports = app
+module.exports =  aplicativo 
+module.exports =  app ;
--- servidor-paraolar/to-do-server/src/controllers/toDoController.js
+++ servidor-paraolar/to-do-server/src/controllers/toDoController.js
@@ -1,67 +1,90 @@
-const tarefasJson = require("../models/tarefas.json")
-const fs = require("fs")
+const  tarefasJson  =  require ( "../models/tarefas.json" )
+const  fs  =  require ( "fs" )
+const  tarefasJson  =  require ( "../models/tarefas.json" ) ;
+const  fs  =  require ( "fs" ) ;
 
-const getAll = (request, response)=>{
-    response.status(200).send(tarefasJson)
+const  getAll  =  ( request ,  response ) => {
+    response.status ( 200 ).send ( tarefasJson )
 }
+ ;
 
-const getById = (request, response) =>{
-    const idRequirido = request.params.id
-    const tarefaFiltrada = tarefasJson.find(tarefa => tarefa.id == idRequirido)
+const  getById  =  ( request ,  response )  => {
+    const  idRequirido  =  request.params.id
+    const  tarefaFiltrada  =  tarefasJson.find ( tarefa  =>  tarefa.id  ==  idRequirido )
 
-    response.status(200).send(tarefaFiltrada)
-}
+    response.status ( 200 ).send ( tarefaFiltrada )
+};
 
-const createTask = (request, response) =>{
-    const descricaoRequirida = request.body.descricao
-    const nomeColaboradorRequirido = request.body.nomeColaborador
+const  createTask  =  ( request ,  response )  => {
+    const  descricaoRequirida  =  request.corpo.descrição 
+    const createTask = (request, response) => {tarefasJson.push ( novaTarefa )
+ fs.writeFile ( "./src/models/tarefas.json", JSON.stringify ( tarefasJson ) ,  'utf8' , function ( err ) {
+         if ( err )  {
+              response.status ( 424 ).send ( { mensagem : err } )
+         }
+     } )
 
-    
+    response.status ( 200 ).send ( novaTarefa )
+} ;
 
-    const novaTarefa ={
-        id: Math.random().toString(32).substr(2,9),
-        dataInclusao: new Date(),
-        concluido: false,
-        descricao: descricaoRequirida,
-        nomeColaborador: nomeColaboradorRequirido
+    const  replaceTask  =  ( request ,  response )  =>  {
+    const  idRequirido  =  pedido.params.id
+    const  newBody  =  pedido.corpo.descrição
+    const  tarefaFiltrada  =  tarefasJson.find ( tarefa  =>  tarefa.id  ==  idRequirido )
+
+    let  replaceTask  =  {
+        id : tarefaFiltrada . id ,
+        dataInclusao : new  Date ( ) ,
+        concluido : verdadeiro ,
+        descricao : newBody ,
+        nomeColaborador : tarefaFiltrada . nomeColaborador
+
     }
+    }
+       const índice =  tarefasJson.indexOf ( tarefaFiltrada )
 
-    tarefasJson.push(novaTarefa)
+    tarefasJson.splice ( index ,  1 ,  replaceTask )
 
-    // fs.writeFile("./src/models/tarefas.json", JSON.stringify(tarefasJson), 'utf8', function(err){
-    //     if(err) {
-    //         return response.status(424).send({message: err})
-    //     }
-    // })
+    fs.writeFile ( "./src/models/tarefas.json" ,  JSON.stringify ( tarefasJson ) ,  'utf8' ,  function ( err ) {
+        if ( err )  {
+             response.status ( 424 ).send ( { mensagem : err } )
+        }
+    } )
 
-    response.status(200).send(novaTarefa)
+    response.status ( 200 ) . json ( [ {
+        "mensagem" : "Tarefa atualizada" ,
+        SubstituiçãoTarefa
+    } ] )
 
-}
+} ;
 
-const deleteTask = (request, response)=>{
-    const idRequirido = request.params.id
-    const tarefaFiltrada = tarefasJson.find(tarefa => tarefa.id == idRequirido)
+    const  deleteTask  =  ( request ,  response ) => {
+    const  tarefaFiltrada  =  pedido.params.eu.id
+    const deleteTask = (request, resposta) => {
+    const  índice  =  tarefasJson.indexOf ( tarefaFiltrada )
+    tarefasJson . splice ( índice ,  1 )
 
-    const indice = tarefasJson.indexOf(tarefaFiltrada)
-    tarefasJson.splice(indice, 1)
+    fs.writeFile ( "./src/models/tarefas.json" ,  JSON.stringify ( tarefasJson ) ,  'utf8' ,  function ( err ) {
+        if ( err )  {
+             response.status ( 424 ).enviar ( { mensagem : err } )
+        }
+    } )
 
-    // fs.writeFile("./src/models/tarefas.json", JSON.stringify(tarefasJson), 'utf8', function(err){
-    //     if(err) {
-    //         return response.status(424).send({message: err})
-    //     }
-    // })
-
-    response.status(200).json([{
-        "mensagem": "Tarefa deletada com sucesso",
+    resposta.status ( 200 ).json ( [ {
+        "mensagem" : "Tarefa deletada com sucesso" ,
         tarefasJson
-    }])
+    } ] )
 
 }
+} ;
 
 
-module.exports ={
-    getAll,
-    getById,
-    createTask,
+module.exports ={    
+    getAll ,
+    getById ,
+    createTask ,
     deleteTask
-}
+} 
+    deleteTask ,
+    substituirTask
+ ; 
