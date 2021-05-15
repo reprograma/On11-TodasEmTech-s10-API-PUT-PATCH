0aa11c8a54f979e284cc90b96143072ad8f5cbf1
Magnolia Medeiros <magnoliamedeiros@gmail.com>
2021-05-15 19:35:58 -0300

    Adicionado a palavra PORTA no console.log

--- servidor-em-aula/server.js
+++ servidor-em-aula/server.js
@@ -2,5 +2,5 @@ const app = require("./src/app")
 const PORT = 8080
 
 app.listen(PORT, ()=>{
-    console.log(`alo alo alo to ligando na ${PORT}`)
+    console.log(`alo alo alo to ligando na PORTA: ${PORT}`)
 })
