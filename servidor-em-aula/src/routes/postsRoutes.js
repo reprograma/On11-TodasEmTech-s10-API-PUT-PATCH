fb55134eb322280848c766ef55bd595bffcc80ad
Magnolia Medeiros <magnoliamedeiros@gmail.com>
2021-05-15 19:37:31 -0300

    Concluido o arquivo routes do posts

--- servidor-em-aula/src/routes/postsRoutes.js
+++ servidor-em-aula/src/routes/postsRoutes.js
@@ -1,7 +1,6 @@
-const express = require ("express")
-const router = express.Router()
-
-const controller = require("../controllers/postsControllers")
+const express = require ("express") // Chama o express
+const router = express.Router() // Executa o Router
+const controller = require("../controllers/postsControllers") // Chama o controller
 
 router.get("/", controller.getAll)
 router.get("/:id", controller.getById)
@@ -14,6 +13,5 @@ router.patch("/updateTitle/:id", controller.updateTitle)
 router.patch("/update/:id", controller.updateAnything)
 
 router.delete("/:id", controller.deletePost)
-
 
 module.exports = router; 
