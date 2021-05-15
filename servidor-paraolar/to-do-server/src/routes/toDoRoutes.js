b615cca311709d5d65739e5a8dd288e7c2d98709
Dani Botelho <danibl26@gmail.com>
2021-05-15 19:20:17 -0300

    Atividade para o lar

--- servidor-paraolar/to-do-server/src/routes/toDoRoutes.js
+++ servidor-paraolar/to-do-server/src/routes/toDoRoutes.js
@@ -7,6 +7,11 @@ router.get("/:id", controller.getById)
 
 router.post("/cadastrar", controller.createTask)
 
+router.put("/:id", controller.replacePost)
+
+router.patch("/:id", controller.updateName)
+router.patch("/update/:id", controller.updateAnything)
+
 router.delete("/:id", controller.deleteTask)
 
 module.exports = router
