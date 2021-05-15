250c038124af8cdbd7866353a5272c2965b180d9
Magnolia Medeiros <magnoliamedeiros@gmail.com>
2021-05-14 23:19:02 -0300

    Configurado o app.js

--- servidor-em-aula/src/app.js
+++ servidor-em-aula/src/app.js
@@ -1,12 +1,11 @@
-const express = require("express")
-const cors = require("cors")
-const posts = require("./routes/postsRoutes")
+const express = require("express") // Chama o express
+const cors = require("cors") // Chama o cors
+const posts = require("./routes/postsRoutes") // Chama as rotas
 
-const app = express()
+const app = express() // Executa o express
 
 app.use(cors())
-app.use(express.json())
-
-app.use("/posts", posts)
+app.use(express.json()) // Faz o parse do body
+app.use("/posts", posts) // Configurando a rota principal
 
 module.exports = app
