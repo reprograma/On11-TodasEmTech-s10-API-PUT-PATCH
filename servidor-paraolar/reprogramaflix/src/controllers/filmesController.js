84d621e2520d5c765e858bde14985e0060a73148
Magnolia Medeiros <magnoliamedeiros@gmail.com>
2021-05-15 22:52:14 -0300

    Concluído o filmesController.js

--- servidor-paraolar/reprogramaflix/src/controllers/filmesController.js
+++ servidor-paraolar/reprogramaflix/src/controllers/filmesController.js
@@ -1,11 +1,14 @@
-const filmes = require("../models/filmes.json") //chamar nosso json
+const filmesJson = require("../models/filmes.json") //chamar nosso json
 
-const getAll = (request, response)=>{ //criar função getAll
-    response.status(200).send(filmes)
+// Retorna todos os filmes
+const getAll = (request, response)=>{
+    response.status(200).send(filmesJson)
 }
+
+// Retorna um filme por id
 const getById = (request, response)=>{
     const idRequerido = request.params.id
-    let idFiltrado = filmes.find(filme => filme.id == idRequerido)
+    let idFiltrado = filmesJson.find(filme => filme.id == idRequerido)
 
     if(idFiltrado == undefined || idRequerido == " "){
         response.status(404).json([{
@@ -16,9 +19,10 @@ const getById = (request, response)=>{
     }   
 }
 
+// Retorna um filme por título
 const getByTitle = (request, response)=>{
     const titulo = request.query.titulo.toLowerCase()
-    const filmeFiltrado = filmes.find(filme => filme.Title.toLowerCase().includes(titulo))
+    const filmeFiltrado = filmesJson.find(filme => filme.Title.toLowerCase().includes(titulo))
 
     if(titulo == "" || filmeFiltrado == undefined){
         response.status(400).json([{
@@ -29,17 +33,18 @@ const getByTitle = (request, response)=>{
     }
 }
 
+// Retorna um filme por gênero
 const getByGenre = (request, response)=>{
-    const generoRequisitado = request.query.genero
+    const generoRequisitado = request.query.genero.toLowerCase()
+    
     let novaLista =[]
    
-    filmes.forEach(filme =>{
+    filmesJson.forEach(filme =>{
         let generoLista = filme.Genre.split(",") 
 
         for(genero of generoLista){
-            
-            if(genero.includes(generoRequisitado)){
-                console.log(filme)
+            if(genero.toLowerCase().includes(generoRequisitado)){
+                // console.log(filme)
                 novaLista.push(filme)
             }
         }
@@ -48,9 +53,121 @@ const getByGenre = (request, response)=>{
     response.status(200).send(novaLista)
 }
 
+// Cria um filme
+const createFilm = (request, response)=>{
+    let tituloRequerido = request.body.Title
+    let anoRequerido = request.body.Year
+    let avaliacaoRequerida = request.body.Rated
+    let liberacaoRequerida = request.body.Released
+    let duracaoRequerida = request.body.Runtime
+    let generoRequerido = request.body.Genre
+    let diretorRequerido = request.body.Director
+    let escritorRequerido = request.body.Writer
+    let atoresRequerido = request.body.Actors
+    let tramaRequerido = request.body.Plot
+    let linguagemRequerida = request.body.Language
+    let paisRequerido = request.body.Country
+    let premiosRequerido = request.body.Awards
+
+    // Criando um novo filme
+    const newFilm = {
+        id: Math.random().toString(32).substr(2,6),
+        Title: tituloRequerido,
+        Year: anoRequerido,
+        Rated: avaliacaoRequerida,
+        Released: liberacaoRequerida,
+        Runtime: duracaoRequerida,
+        Genre: generoRequerido,
+        Director: diretorRequerido,
+        Writer: escritorRequerido,
+        Actors: atoresRequerido,
+        Plot: tramaRequerido,
+        Language: linguagemRequerida,
+        Country: paisRequerido,
+        Awards: premiosRequerido
+    }
+
+    // Adicionando o novo filme ao json
+    filmesJson.push(newFilm)
+
+    response.status(201).json([{
+        "mensagem": "Filme criado com sucesso",
+        newFilm
+    }])
+}
+
+// Atualiza um filme existente
+const replaceFilm = (request, response)=>{
+    const idRequerido = request.params.id
+    const filmeBody = request.body
+    const filmeFiltrado = filmesJson.find(filme => filme.id == idRequerido)
+    
+    const filmeAtualizado = {
+        id: filmeFiltrado.id,
+        Title: filmeBody.Title,
+        Year: filmeBody.Year,
+        Rated: filmeBody.Rated,
+        Released: filmeBody.Released,
+        Runtime: filmeBody.Runtime,
+        Genre: filmeBody.Genre,
+        Director: filmeBody.Director,
+        Writer: filmeBody.Writer,
+        Actors: filmeBody.Actors,
+        Plot: filmeBody.Plot,
+        Language: filmeBody.Language,
+        Country: filmeBody.Country,
+        Awards: filmeBody.Awards
+    }
+
+    const indice = filmesJson.indexOf(filmeFiltrado)
+    filmesJson.splice(indice, 1, filmeAtualizado)
+
+    response.status(200).json([{
+        "mensagem": "Filme atualizado com sucesso",
+        filmeAtualizado
+    }])
+}
+
+// Atualiza qualquer coisa
+const updateAnything = (request, response)=>{
+    const idRequerido = request.params.id
+    const atualizaBody = request.body
+    const filmeFiltrado = filmesJson.find(filme => filme.id == idRequerido)
+
+    const listaChaves = Object.keys(atualizaBody)
+    
+    listaChaves.forEach((chave) => {
+        filmeFiltrado[chave] = atualizaBody[chave]
+    })
+
+    response.status(200).json([{
+        "mensagem": "Filme atualizado com sucesso",
+        filmeFiltrado
+    }])
+}
+
+// Deleta um filme por id
+const deleteFilm = (request, response)=>{
+    const idRequerido = request.params.id
+    const filmeFiltrado = filmesJson.find(filme => filme.id == idRequerido)
+    const indice = filmesJson.indexOf(filmeFiltrado)
+
+    filmesJson.splice(indice, 1)
+
+    response.status(200).json([{
+        "mensagem": "Filme deletado com sucesso",
+        filmesJson
+    }])
+
+}
+
 module.exports = { //exportando as funções
     getAll,
     getById,
     getByTitle,
-    getByGenre
+    getByGenre,
+    createFilm,
+    replaceFilm,
+    updateAnything,
+    deleteFilm
 }
