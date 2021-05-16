f63c04586d09f15f8ec022487655ff4d00dc51fa
Mariana Lima <marilimabrito13@gmail.com>
2021-05-16 09:26:16 -0300

    atividade semana 10

--- servidor-paraolar/reprogramaflix/src/controllers/filmesController.js
+++ servidor-paraolar/reprogramaflix/src/controllers/filmesController.js
@@ -48,9 +48,69 @@ const getByGenre = (request, response)=>{
     response.status(200).send(novaLista)
 }
 
-module.exports = { //exportando as funções
+const createTask = (request, response) =>{
+    const titleRequerido = request.body.Title
+    const anoRequerido = request.body.Year
+
+
+        console.log(titleRequerido)
+        console.log(anoRequerido)
+
+    const novaTarefa ={
+        id: Math.random().toString(32).substring(2,9),
+        Title: titleRequerido,
+        Year: anoRequerido
+    }
+
+      filmes.push(novaTarefa)
+
+      response.status(201).json([{
+          "mensagem": "filme inserido",
+          novaTarefa
+      }])
+
+}
+
+const replaceFilme = (request, response )=>{
+    const idRequerido = request.params.id
+    let postAtualizado = request.body
+    const idFiltrado = filmes.find(filme => filme.id == idRequerido)
+    const indice = filmes.indexOf(idFiltrado)
+
+    console.log(postAtualizado)
+
+    postAtualizado.id = idRequerido
+    
+    filmes.splice(indice, 1, postAtualizado)
+      
+    response.status(200).json([{
+        "menssagem": "Post atualizado com sucesso",
+         postAtualizado
+    }])
+
+
+}
+const deleteTask = (request, response) =>{
+    const idRequerido = request.params.id
+    let idFiltrado = filmes.find(filme => filme.id == idRequerido)
+    const indice = filmes.indexOf(idFiltrado)
+    filmes.splice(indice, 1)
+    response.status(200).json([{
+        "menssagem": "Filme deletado com sucesso",
+        filmes
+    }])
+}
+
+
+
+module.exports ={
     getAll,
     getById,
     getByTitle,
-    getByGenre
-}
+    getByGenre,
+    createTask,
+    deleteTask,
+    replaceFilme
+
+} //exportando as funções
+    
--- servidor-paraolar/reprogramaflix/src/routes/filmesRoutes.js
+++ servidor-paraolar/reprogramaflix/src/routes/filmesRoutes.js
@@ -7,6 +7,11 @@ router.get("/todos", controller.getAll)//configurando continuação da rota e di
 router.get("/title", controller.getByTitle)
 router.get("/genre", controller.getByGenre)
 router.get("/:id", controller.getById)
+router.post("/cadastrar", controller.createTask)
+router.delete("/:id", controller.deleteTask)
+router.put("/:id", controller.replaceFilme)
+
+
 
 
 module.exports = router //exportando o router
