5a9959b52dd05a2a4cfd985524600909e98f2599
Thay Mendes <thay489@gmail.com>
2021-06-28 17:41:02 -0500

    esxercicios semana 10

--- servidor-paraolar/reprogramaflix/src/routes/filmesRoutes.js
+++ servidor-paraolar/reprogramaflix/src/routes/filmesRoutes.js
@@ -1,12 +1,10 @@
-const controller  = require("../controllers/filmesController")//chama o controler
-
-const express = require("express")//chamando o express 
-const router = express.Router()//executando Router
-
-router.get("/todos", controller.getAll)//configurando continuação da rota e dizendo q ela vai usar a função getAll que esta no controller
 router.get("/title", controller.getByTitle)
 router.get("/genre", controller.getByGenre)
 router.get("/:id", controller.getById)
+router.post("/cadastrar", controller.createTask)
+router.delete("/:id", controller.deleteTask)
+router.put("/:id", controller.replaceFilme)
+
 
 
 module.exports = router //exportando o router
